2ac94e58bcb9beeef5dd815353899a592138f1c0
Lalan Desai <lalandesai@outlook.com>
2022-07-13 11:52:44 +0530

    Add files via upload

---
+++ Prog-14/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+
+string text = "aa bb cc dd";
+
+var list = text.GroupBy(c => c).Select(g => new { Key = g.Key, Count = g.Count() });
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
---
+++ Prog-15/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+
+string text = "aa bb cc dd";
+
+var list = text.Split(' ').GroupBy(c => c).Select(g => new { Key = g.Key, Count = g.Count() });
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
