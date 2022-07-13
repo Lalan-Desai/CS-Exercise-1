55c2ebd0774328ad22385edbd4b95ad938070528
Lalan Desai <lalandesai@outlook.com>
2022-07-13 11:25:10 +0530

    Add files via upload

---
+++ Prog-3/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = 0;
+int y = 0;
+int counter = 0;
+
+Console.Write("Enter x: ");
+x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter y: ");
+y = int.Parse(Console.ReadLine()!);
+
+for (int i = x; i<= y; i++)
+{
+    if (i%5 == 0)
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine($"Counter: {counter}");
---
+++ Prog-4/Program.cs
@@ -0,0 +1,17 @@
+﻿int x = 0;
+int y = 0;
+
+Console.Write("Enter x: ");
+x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter y: ");
+y = int.Parse(Console.ReadLine()!);
+
+if (x > y)
+{
+    Console.WriteLine($"x({x}) is geater than y({y}).");
+}
+else
+{
+    Console.WriteLine($"y({y}) is geater than x({x}).");
+}
